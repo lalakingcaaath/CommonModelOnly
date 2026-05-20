1d57658f5d46a7a0dcd286bd6bd140656e814cb4
jrofuoco <joshuaorlina08@gmail.com>
2026-05-20 11:20:08 +0800

    Initial commit for common_model module

---
+++ src/main/java/com/jimac/common_model/ParkerType/ParkerModel.java
@@ -0,0 +1,31 @@
+package com.jimac.common_model.ParkerType;
+
+public class ParkerModel {
+    private String parkerName;
+    private int parkerCount;
+    private double parkerIncome;;
+
+    public String getParkerName() {
+        return parkerName;
+    }
+
+    public void setParkerName(String parkerName) {
+        this.parkerName = parkerName;
+    }
+
+    public int getParkerCount() {
+        return parkerCount;
+    }
+
+    public void setParkerCount(int parkerCount) {
+        this.parkerCount = parkerCount;
+    }
+
+    public double getParkerIncome() {
+        return parkerIncome;
+    }
+
+    public void setParkerIncome(double parkerIcome) {
+        this.parkerIncome = parkerIcome;
+    }
+}
---
+++ src/main/java/com/jimac/common_model/VehicleType/VehicleModel.java
@@ -0,0 +1,31 @@
+package com.jimac.common_model.VehicleType;
+
+public class VehicleModel {
+    private String vehicleName;
+    private int vehicleCount;
+    private double vehicleIncome;
+
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+
+    public int getVehicleCount() {
+        return vehicleCount;
+    }
+
+    public void setVehicleCount(int vehicleCount) {
+        this.vehicleCount = vehicleCount;
+    }
+
+    public double getVehicleIncome() {
+        return vehicleIncome;
+    }
+
+    public void setVehicleIncome(double vehicleIncome) {
+        this.vehicleIncome = vehicleIncome;
+    }
+}
