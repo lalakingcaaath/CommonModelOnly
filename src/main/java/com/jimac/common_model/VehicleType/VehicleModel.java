45a2661c0d2e320244959c643cd19a48751eefb9
jrofuoco <joshuaorlina08@gmail.com>
2026-05-21 09:23:27 +0800

    tangina

--- src/main/java/com/jimac/common_model/VehicleType/VehicleModel.java
+++ src/main/java/com/jimac/common_model/VehicleType/VehicleModel.java
@@ -1,15 +1,9 @@
 package com.jimac.common_model.VehicleType;
 
 public class VehicleModel {
-<<<<<<< HEAD
     String vehicleName;
     int vehicleCount = 0;
     Double vehicleIncome = (double) 0.0F;
-=======
-    private String vehicleName;
-    private int vehicleCount;
-    private double vehicleIncome;
->>>>>>> 1d57658f5d46a7a0dcd286bd6bd140656e814cb4
 
     public String getVehicleName() {
         return vehicleName;
@@ -27,19 +21,11 @@ public class VehicleModel {
         this.vehicleCount = vehicleCount;
     }
 
-<<<<<<< HEAD
     public Double getVehicleIncome() {
         return vehicleIncome;
     }
-
     public void setVehicleIncome(Double vehicleIncome) {
-=======
-    public double getVehicleIncome() {
-        return vehicleIncome;
-    }
 
-    public void setVehicleIncome(double vehicleIncome) {
->>>>>>> 1d57658f5d46a7a0dcd286bd6bd140656e814cb4
         this.vehicleIncome = vehicleIncome;
     }
 }
