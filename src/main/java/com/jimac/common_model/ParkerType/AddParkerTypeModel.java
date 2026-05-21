bad5da92162af63c2d35edf5edf3b22ce8de2828
jrofuoco <joshuaorlina08@gmail.com>
2026-05-21 11:30:54 +0800

    Renamed AddParkerTypeModel and AddVehicleTypeModel

--- src/main/java/com/jimac/common_model/ParkerType/AddParkerTypeModel.java
+++ src/main/java/com/jimac/common_model/ParkerType/AddParkerTypeModel.java
@@ -1,13 +1,14 @@
-package com.jimac.common_model.VehicleType;
+package com.jimac.common_model.ParkerType;
 
 import android.net.Uri;
 
-public class VehicleTypeModel {
+public class AddParkerTypeModel {
     public String name;
     public Uri imageUri;
+
     public String imageBase64;
 
-    public VehicleTypeModel(String name, Uri imageUri, String imageBase64) {
+    public AddParkerTypeModel(String name, Uri imageUri, String imageBase64) {
         this.name = name;
         this.imageUri = imageUri;
         this.imageBase64 = imageBase64;
--- src/main/java/com/jimac/common_model/VehicleType/AddVehicleTypeModel.java
+++ src/main/java/com/jimac/common_model/VehicleType/AddVehicleTypeModel.java
@@ -1,14 +1,13 @@
-package com.jimac.common_model.ParkerType;
+package com.jimac.common_model.VehicleType;
 
 import android.net.Uri;
 
-public class ParkerTypeModel {
+public class AddVehicleTypeModel {
     public String name;
     public Uri imageUri;
-
     public String imageBase64;
 
-    public ParkerTypeModel(String name, Uri imageUri, String imageBase64) {
+    public AddVehicleTypeModel(String name, Uri imageUri, String imageBase64) {
         this.name = name;
         this.imageUri = imageUri;
         this.imageBase64 = imageBase64;
